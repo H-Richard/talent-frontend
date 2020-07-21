58a5b2545c7c3bc1dea07726f3166fae82633f9e
liubryann <32988302+liubryann@users.noreply.github.com>
2020-07-21 13:30:18 -0400

    Update src/components/Footer/index.tsx
    Combined imports
    
    Co-authored-by: Richard <r.hong@pm.me>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import { Typography, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
 
 function FooterInfo() {
   return (
