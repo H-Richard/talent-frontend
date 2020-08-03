682081aabad8a4487f23547edbcebd28d3deffc1
Jason Hu <jason.h.7249@gmail.com>
2020-08-03 18:00:32 -0400

    Added date formatter/calculator. Fixed typo in Footer.

--- src/pages/Job Details/index.tsx
+++ src/pages/Job Details/index.tsx
@@ -25,9 +25,27 @@ type JobProps = {
   }
 };
 
-const parseDate = (date: string): string => {
-  const parsedDate: Date = new Date(Date.parse(date));
-  return parsedDate.toDateString();
+const getDateDiff = (newDateStr: string, isExpiryDate: boolean = false): string => {
+  const dateToday: Date = new Date();
+  const dateTodayInMs: number = dateToday.valueOf() - (dateToday.valueOf() % 86400000);
+  const newDateInMs: number = new Date(newDateStr).valueOf();
+
+  const prefix: string = (isExpiryDate) ? 'in ' : '';
+  const suffix: string = (isExpiryDate) ? '' : ' ago';
+
+  const millisecondsDiff: number = Math.abs(newDateInMs - dateTodayInMs);
+  const daysDiff: number = Math.ceil(millisecondsDiff / 86400000);
+
+  let returnString: string;
+  if (daysDiff < 1) {
+    returnString = 'today';
+  } else if (daysDiff === 1) {
+    returnString = (isExpiryDate) ? 'tomorrow' : 'yesterday';
+  } else {
+    returnString = `${prefix}${daysDiff} days${suffix}`;
+  }
+
+  return returnString;
 };
 
 const JobDetails: FC<JobProps> = ({ job }: JobProps) => (
@@ -37,9 +55,9 @@ const JobDetails: FC<JobProps> = ({ job }: JobProps) => (
         <Typography variant="h2">{job.title}</Typography>
       </Grid>
       <Grid item md={4}>
-        <Typography variant="h6">{`Posted: ${parseDate(job.createdAt)}`}</Typography>
-        <Typography variant="h6">{`Last updated: ${parseDate(job.updatedAt)}`}</Typography>
-        <Typography variant="h6">{`Expires: ${parseDate(job.expiresAt)}`}</Typography>
+        <Typography variant="h6">{`Posted ${getDateDiff('2020-07-19')}`}</Typography>
+        <Typography variant="h6">{`Last updated ${getDateDiff(job.updatedAt)}`}</Typography>
+        <Typography variant="h6">{`Expires ${getDateDiff(job.expiresAt, true)}`}</Typography>
       </Grid>
     </Grid>
   </Container>
