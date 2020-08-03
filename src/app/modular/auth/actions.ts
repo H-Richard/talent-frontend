bb8785f75a4f59d57f5c58d050f6f2dfb77f763d
ivinable2000 <44382576+ivinable2000@users.noreply.github.com>
2020-08-03 18:30:55 -0400

    Feature/signup page (#32)
    * updated toggleMode function to toggle theme item in local storage
    
    * lint and type fixes
    
    * Added use of event listener to window
    
    * lint fixes
    
    * Partial changes in removing state logic for theme Type
    
    * feat(theme): add useThemeMode hook
    
    * style(header component  and app.tsx): lint issues fixed
    
    close #21
    
    * feat(signup): add signup page to /signup
    
    Add signup page at /signup for users to input name, email, and password for signup purposes. Current
    form takes no action, it is only the format of the page.
    
    * feat(signup): add signup logic to create user given name, email and password and redirect to home
    
    Add validation logic to signup form for proper name, email and passwords, use redux to create user
    using form values and redirect user to home after signup. Save user info in local storage
    
    #2
    
    * refactor(signup): change console.log to console.error
    
    Change console.log to console.error
    
    * feat(signup): add signup page exclusive actions, reducer changes, error handling, edit functions
    
    Added actions for signup page start, success and errors. Edited reducer to act on these actions.
    Added functions for signup success and failure
    
    #2
    
    * feat(signup): change icon, error handling, code styling
    
    Change icon on top of signup page, clear email and password fields on signup failure, reroute to
    home if signed in
    
    closes #2
    
    Co-authored-by: XiangQian Hong <XiangQian.Hong@ibm.com>

--- src/app/modular/auth/actions.ts
+++ src/app/modular/auth/actions.ts
@@ -10,6 +10,9 @@ export const LOGIN_START = 'auth/loginStart';
 export const LOGIN_SUCCESS = 'auth/loginSuccess';
 export const LOGIN_ERROR = 'auth/loginError';
 export const CLEAR_ERROR = 'auth/clearError';
+export const SIGNUP_START = 'auth/signupStart';
+export const SIGNUP_SUCCESS = 'auth/signupSuccess';
+export const SIGNUP_ERROR = 'auth/signupError';
 
 export const loginStart = () => action(LOGIN_START);
 
@@ -19,6 +22,11 @@ export const loginSuccess = (user: User) => action(LOGIN_SUCCESS, { user });
 
 export const clearError = () => action(CLEAR_ERROR);
 
+export const signupStart = () => action(SIGNUP_START);
+
+export const signupSuccess = (user: User) => action(SIGNUP_SUCCESS, { user });
+
+export const signupError = (error: string) => action(SIGNUP_ERROR, { error });
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const login = (
   {
@@ -50,5 +58,45 @@ export const login = (
     console.error(err);
     dispatch(loginError(err.message as string));
     onLoginFailure();
+  }
+});
+
+export const signup = (
+  {
+    firstName,
+    lastName,
+    email,
+    password,
+    onSignupSuccess,
+    onSignupFailure,
+  }:
+  {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    onSignupSuccess: VoidFunction,
+    onSignupFailure: VoidFunction
+  },
+): ThunkAction<void, RootState, unknown, Action> => (async (dispatch) => {
+  dispatch(signupStart());
+  try {
+    const { token, user }: any = await request.unauthenticatedRequest({
+      method: 'POST',
+      url: 'users',
+      body: {
+        firstName, lastName, email, password,
+      },
+    });
+    saveJWT(token);
+    user.updatedAt = new Date(user.updatedAt);
+    saveCurrentUser(user);
+    dispatch(signupSuccess(user));
+    onSignupSuccess();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    dispatch(signupError(err.message as string));
+    onSignupFailure();
   }
 });
---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,205 @@
+import React from 'react';
+import {
+  Form, FormikProps, Field, withFormik,
+} from 'formik';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { TextField } from 'formik-material-ui';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  Link,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import * as Yup from 'yup';
+import { withRouter } from 'react-router';
+import { RouteComponentProps } from 'react-router-dom';
+
+import authDuck from '../../app/modular/auth';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(9),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+    alignItems: 'center',
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  input: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  formContainer: {
+    padding: theme.spacing(2),
+    marginTop: theme.spacing(4),
+  },
+}));
+
+interface ConnectedActions {
+  signup: typeof authDuck.actions.signup;
+}
+
+interface FormValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  reenter_password: string;
+}
+
+type Props = ConnectedActions & FormikProps<FormValues> & RouteComponentProps;
+const Signup: React.FC<Props> = ({ handleSubmit }: Props) => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <BorderColorIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Paper className={classes.formContainer} elevation={3}>
+            <Form onSubmit={handleSubmit} className={classes.form}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    name="firstName"
+                    type="firstName"
+                    label="First Name"
+                    variant="outlined"
+                    autoComplete="off"
+                    className={classes.input}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    name="lastName"
+                    type="lastName"
+                    label="Last Name"
+                    variant="outlined"
+                    autoComplete="off"
+                    className={classes.input}
+                  />
+                </Grid>
+              </Grid>
+              <Field
+                component={TextField}
+                name="email"
+                type="email"
+                label="Email"
+                variant="outlined"
+                autoComplete="off"
+                className={classes.input}
+              />
+              <Field
+                component={TextField}
+                variant="outlined"
+                name="password"
+                type="password"
+                label="Password"
+                autoComplete="off"
+                className={classes.input}
+              />
+              <Field
+                component={TextField}
+                variant="outlined"
+                name="reenter_password"
+                type="password"
+                label="Re-enter Password"
+                autoComplete="off"
+                className={classes.input}
+              />
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+            </Form>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Paper>
+        </div>
+      </CssBaseline>
+    </Container>
+  );
+};
+
+const mapPropsToDispatch = {
+  signup: authDuck.actions.signup,
+};
+
+const enhanceForm = withFormik({
+  validateOnMount: true,
+  mapPropsToValues: () => ({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    reenter_password: '',
+  }),
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string().min(1).required('First Name is required'),
+    lastName: Yup.string().min(1).required('Last Name is required'),
+    email: Yup.string().min(1).required('Enter your email').email('Enter a valid email'),
+    password: Yup.string().min(8, 'Password must contain at least 8 characters').required('Enter your password'),
+    reenter_password: Yup.string().required('Confirm your password').oneOf([Yup.ref('password')], 'Password does not match'),
+  }),
+  handleSubmit: (
+    {
+      firstName,
+      lastName,
+      email,
+      password,
+    }: FormValues,
+    { props: { signup }, setSubmitting, setFieldValue }:
+    { props: Props, setSubmitting: Function, setFieldValue: Function },
+  ) => {
+    signup({
+      firstName,
+      lastName,
+      email,
+      password,
+      onSignupSuccess() { setSubmitting(false); window.location.href = '/home'; },
+      onSignupFailure() { setSubmitting(false); setFieldValue('email', '', false); setFieldValue('password', '', false); setFieldValue('reenter_password', '', false); },
+    });
+  },
+  displayName: 'signup',
+});
+
+export default compose<React.FC>(
+  connect(null, mapPropsToDispatch),
+  enhanceForm,
+  withRouter,
+)(Signup);
