f3c418bbe78d6de0f57a94bc855435803b53088d
ivinable2000 <ivinable2000@gmail.com>
2020-08-08 10:45:25 -0400

    feat(postduck): [WIP]: Add post thunk

--- src/app/modular/post/actions.ts
+++ src/app/modular/post/actions.ts
@@ -1,4 +1,7 @@
-import { action } from 'typesafe-actions';
+import { action, Action } from 'typesafe-actions';
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from '../../types';
+import * as request from '../../../client';
 
 export const LOADING_START = 'post/loadingStart';
 export const LOADING_SUCCESS = 'post/loadingSuccess';
@@ -9,3 +12,20 @@ export const loadingStart = () => action(LOADING_START);
 export const loadingError = () => action(LOADING_ERROR);
 
 export const loadingSuccess = () => action(LOADING_SUCCESS);
+
+export const getPosts = (): ThunkAction<void, RootState, unknown, Action> => (async (dispatch) => {
+  dispatch(loadingStart());
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const posts: any = await request.unauthenticatedRequest({
+      method: 'GET',
+      url: 'posts',
+    });
+    // Save posts in store
+    dispatch(loadingSuccess());
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    dispatch(loadingError());
+  }
+});
