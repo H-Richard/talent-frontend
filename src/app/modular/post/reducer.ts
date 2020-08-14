c9a2ed66f0f734e19c9086ecf54b5ec81ec8140e
ivinable2000 <44382576+ivinable2000@users.noreply.github.com>
2020-08-14 11:38:46 -0400

    Add createPost Thunk (#42)
    * feat(post duck): added createPost thunk action
    
    Added createPost thunk action that creates new post and saves it to existing dictionary of posts
    
    #30
    
    * feat(postduck): add to reducer and type to handle error actions
    
    Add error actions, and use reducer to change error in state
    
    #30

--- src/app/modular/post/reducer.ts
+++ src/app/modular/post/reducer.ts
@@ -24,9 +24,28 @@ const reducer: PostReducer = produce(
       case actions.SAVE_POSTS:
         state.posts = action.payload.posts;
         break;
+      case actions.ADD_POST:
+        // eslint-disable-next-line no-case-declarations
+        const { post } = action.payload;
+        state.posts[post.id] = post;
+        state.loading = false;
+        break;
       case actions.LOADING_SUCCESS:
+        state.loading = false;
+        break;
       case actions.LOADING_ERROR:
         state.loading = false;
+        switch (action.payload.error.slice(-3)) {
+          case '401':
+            state.error = 'Unauthorized';
+            break;
+          default:
+            state.error = action.payload.error;
+            break;
+        }
+        break;
+      case actions.CLEAR_ERROR:
+        state.error = undefined;
         break;
       default:
         break;
